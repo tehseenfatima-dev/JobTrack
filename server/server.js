8eb9fe9423080dd7a1d6930bf6c23c0bcb55e20a
mashaaa00000 <Mahaaa87659@gmail.com>
2026-07-25 21:12:23 -0700

    Update production API and CORS

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "job-track-fi59-1duo4h6zp-team-pro12.vercel.app",
         credentials: true
     })
 );
